edb16e00e6221592d4f9699defe0de49079d8ba2
nofirst <1025926969@qq.com>
2026-02-08 12:45:56 +0800

    add test: update to a basic test

--- src/test/java/com/nofirst/spring/tdd/zhihu/startup/integration/ViewQuestionsTests.java
+++ src/test/java/com/nofirst/spring/tdd/zhihu/startup/integration/ViewQuestionsTests.java
@@ -1,20 +1,13 @@
 package com.nofirst.spring.tdd.zhihu.startup.integration;
 
-import com.nofirst.spring.tdd.zhihu.startup.factory.QuestionFactory;
-import com.nofirst.spring.tdd.zhihu.startup.mbg.mapper.QuestionMapper;
-import com.nofirst.spring.tdd.zhihu.startup.mbg.model.Question;
-import com.nofirst.spring.tdd.zhihu.startup.mbg.model.QuestionExample;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,9 +20,6 @@ public class ViewQuestionsTests extends BaseContainerTest {
     @Autowired
     private WebApplicationContext context;
 
-    @Autowired
-    private QuestionMapper questionMapper;
-
 
     @BeforeAll
     public void setupMockMvc() {
@@ -38,29 +28,14 @@ public class ViewQuestionsTests extends BaseContainerTest {
                 .build();
     }
 
-    @BeforeEach
-    public void setupTestData() {
-        QuestionExample example = new QuestionExample();
-        // 空条件，匹配所有数据，等价于delete * from question
-        example.createCriteria();
-        questionMapper.deleteByExample(example);
-    }
-
     @Test
     void user_can_view_questions() throws Exception {
         // given
-        Question question = QuestionFactory.createQuestion();
-        questionMapper.insert(question);
 
         // when
-        MvcResult result = this.mockMvc.perform(get("/questions?pageIndex=1&pageSize=10"))
+        this.mockMvc.perform(get("/questions"))
+                // then
                 .andExpect(status().isOk()).andReturn();
-
-        String json = result.getResponse().getContentAsString();
-
-        // then
-        assertThat(json).contains(question.getTitle());
-        assertThat(json).contains(question.getContent());
     }
 
 }
