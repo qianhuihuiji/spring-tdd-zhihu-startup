e5542f1b48b7600a0b725ea726ed62cd540f4c92
qianhuihuiji <1025926969@qq.com>
2026-02-26 12:38:19 +0800

    更新 spring security 配置并启用方法安全

--- src/main/java/com/nofirst/spring/tdd/zhihu/startup/config/SecurityConfig.java
+++ src/main/java/com/nofirst/spring/tdd/zhihu/startup/config/SecurityConfig.java
@@ -6,9 +6,11 @@ import com.nofirst.spring.tdd.zhihu.startup.security.JwtAuthenticationFilter;
 import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,7 +22,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
+@ComponentScan("com.nofirst.spring.tdd.zhihu.startup.security")
 public class SecurityConfig {
 
     private JwtAuthenticationFilter jwtAuthenticationFilter;
