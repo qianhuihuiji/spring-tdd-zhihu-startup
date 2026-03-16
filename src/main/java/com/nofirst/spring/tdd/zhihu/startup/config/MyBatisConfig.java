0c1358ea37866f5bc5242cf20d47f337468fe037
qianhuihuiji <1025926969@qq.com>
2026-03-16 11:00:33 +0800

    Remove unused package from MyBatis mapper scan

--- src/main/java/com/nofirst/spring/tdd/zhihu/startup/config/MyBatisConfig.java
+++ src/main/java/com/nofirst/spring/tdd/zhihu/startup/config/MyBatisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * MyBatis配置类
  */
 @Configuration
-@MapperScan({"com.nofirst.spring.tdd.zhihu.startup.mbg.mapper", "com.nofirst.spring.tdd.zhihu.startup.dao"})
+@MapperScan({"com.nofirst.spring.tdd.zhihu.startup.mbg.mapper"})
 public class MyBatisConfig {
 }
 
